0374e7900f56431aad857dfe15cdafa90ee3d84a
Sami Horttanainen <sami1horttanainen@gmail.com>
2023-01-13 09:22:04 +0200

    Check if app deployment fails as intented

--- app.js
+++ app.js
@@ -10,5 +10,13 @@ app.use(express.static('dist'))
 app.listen(PORT, () => {
   console.log('server started on port 5000')
 })
+app.get('/health', (_req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
+  res.send('ok')
+})
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
 
 /* eslint-enable*/
